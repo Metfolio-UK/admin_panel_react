d5420e54d06504f1b080231fafda2828743b2e5b
shyam-162001 <lionelandresmessi162002@gmail.com>
2023-02-20 15:54:22 +0530

    Added holdings,charts and Tables

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -7,6 +7,13 @@ import Navbar from '../components/navbar';
 import SidebarFixed from '../components/sidebarFixed';
 import SidebarDrawer from '../components/sidebarDrawer';
 import SellOrders from '../components/sellOrders';
+import BuyOrders from '../components/BuyOrders';
+import RecurringOrders from '../components/RecurringOrders';
+import { ChartsWidget1 } from '../components/Chart';
+import { InvReport } from '../components/InventoryReport';
+import AdminHolding from '../components/AdminHoldings';
+import CustomerHolding from '../components/CustomerHoldings';
+
 const { Content, Footer } = Layout;
 
 
@@ -32,7 +39,21 @@ const Home = (props) => {
         <Content>
           <Navbar width={props.width} showDrawer={showDrawer}/>
           <div style={{height:'20px'}}/>
-          <div style={{ padding:'12px' }}>
+          <div style={{ padding:'20px' }}>
+            <div className='row'>
+              <div className='col'>
+              <AdminHolding className='mb-5 mb-xl-8'/>
+              <CustomerHolding className='mb-5 mb-xl-8'/>
+              </div>
+              <div className='col'>
+              <InvReport className='mb-5 mb-xl-8'/>
+            <ChartsWidget1 className='mb-5 mb-xl-8'/>
+            </div>
+            </div>
+            
+            
+            <BuyOrders className='mb-5 mb-xl-8'/>
+            <RecurringOrders className='mb-5 mb-xl-8'/>
             <SellOrders className='mb-5 mb-xl-8'/>
           </div>
         </Content>
