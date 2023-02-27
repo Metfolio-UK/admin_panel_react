338c0a0188473fb1fb8830e915fe105d4c7d59ee
shyam-162001 <lionelandresmessi162002@gmail.com>
2023-02-27 16:58:29 +0530

    added AuthPage

--- src/_metronic/assets/ts/_utils/index.ts
+++ src/_metronic/assets/ts/_utils/index.ts
@@ -1,12 +1,12 @@
 // Types
-export * from './models/OffsetModel'
-export * from './models/ViewPortModel'
+export * from './models/OffsetModel.ts'
+export * from './models/ViewPortModel.ts'
 // Helpers
-export * from './DomHelpers'
-export * from './_TypesHelpers'
+export * from './DomHelpers.ts'
+export * from './_TypesHelpers.ts'
 // Basic
-export * from './_DataUtil'
-export * from './_DOMEventHandlerUtil'
-export * from './_ElementStyleUtil'
-export * from './ElementAnimateUtil'
-export * from './EventHandlerUtil'
+export * from './_DataUtil.ts'
+export * from './_DOMEventHandlerUtil.ts'
+export * from './_ElementStyleUtil.ts'
+export * from './ElementAnimateUtil.ts'
+export * from './EventHandlerUtil.ts'
